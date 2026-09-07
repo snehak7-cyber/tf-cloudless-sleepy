e64f46a903781e87ad686150481e4a14154a53fe
snehak7-cyber <snehakkaran.7@gmail.com>
2026-09-07 15:52:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
-data "template_file" "test" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var}"
-
-  vars = {
-    sample_var = var.sample_var
-  }
+locals {
+  template_result = "Hello, I am a template. My sample_var value = ${var.sample_var}"
 }
 
 resource "null_resource" "sleep" {
